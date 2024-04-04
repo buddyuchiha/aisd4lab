3202f66ae66edf6139f45a5f4984be3516d796c0
afrodetka <Pro100biv@mail.ru>
2024-04-04 19:12:30 +0400

    Реализация очистки, копирования, присваивания, конструктора, деструктора

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <random>
 using namespace std;
 
 template <typename T>
@@ -7,7 +8,7 @@ struct Node {
 	T _data;
 	Node* _right;
 	Node* _left;
-	Node(T data, Node* left = nullptr, Node* right = nullptr): _data(data), _right(right), _left(left);
+	Node(T data) : _data(data), _right(nullptr), _left(nullptr) {};
 };
 
 template <typename T>
@@ -15,23 +16,49 @@ class Set {
 public:
 	Set();
 	~Set();
+	Set(const Set& other);
 	void print();
 	bool insert(T _data);
 	bool contains(T _data);
 	bool erase(T _data);
 private:
 	Node<T>* _root;
-	void clear(){}
+	void clear(Node* _Node){
+		if (_Node != nullptr) {
+			clear(_Node->left);
+			clear(_Node->right);
+			delete _Node;
+		}
+		_root = nullptr;
+	}
+	void copy(Node*& to, Node* from){
+		if (from) {
+			to = new Node(from->data);
+			copy(to->left, from->left);
+			copy(to->right, from->right);
+		}
+	}
+	Set& operator=(const Set& other) {
+		if (this != &other) {
+			clear(_root);
+			copy(_root, other._root);
+		}
+		return *this;
+	}
 };
 
 template<typename T>
-Set<T>::Set()
-{
+Set<T>::Set(): _root(nullptr){}
+
+template<typename T>
+Set<T>::Set(const Set& other): _root(nullptr){
+	copy(_root, other._root)
 }
 
 template<typename T>
 Set<T>::~Set()
 {
+	clear(_root)
 }
 
 template<typename T>
@@ -55,4 +82,8 @@ template<typename T>
 bool Set<T>::erase(T _data)
 {
 	return false;
+}
+
+int main() {
+
 }
